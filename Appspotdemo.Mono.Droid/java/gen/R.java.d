2f692fae95f06d3a551539a24c7df4afd60c0d8f
Kenneth Thorman <kenneth.thorman@appinux.com>
2013-12-24 09:08:06 +0100

    Adding the Appspot demo src - build action none

---
+++ Appspotdemo.Mono.Droid/java/gen/R.java.d
@@ -0,0 +1,7 @@
+/home/user1/WebRTCDemo/trunk/talk/examples/android/gen/org/appspot/apprtc/R.java \
+ : /home/user1/WebRTCDemo/trunk/talk/examples/android/res/drawable-xhdpi/ic_launcher.png \
+/home/user1/WebRTCDemo/trunk/talk/examples/android/res/drawable-ldpi/ic_launcher.png \
+/home/user1/WebRTCDemo/trunk/talk/examples/android/res/drawable-mdpi/ic_launcher.png \
+/home/user1/WebRTCDemo/trunk/talk/examples/android/res/values/strings.xml \
+/home/user1/WebRTCDemo/trunk/talk/examples/android/res/drawable-hdpi/ic_launcher.png \
+/home/user1/WebRTCDemo/trunk/talk/examples/android/bin/AndroidManifest.xml \
